39f84b1a356deb43d03832766d0a10f0ecae42c0
Saket789 <56218641+Saket789@users.noreply.github.com>
2020-04-29 15:31:52 +0530

    Add files via upload

---
+++ Same_gcd.cpp
@@ -0,0 +1,81 @@
+
+#include <bits/stdc++.h>
+using namespace std ; 
+#define ll              long long int
+// #define cini(x)	        scanf("%lld",&x)
+// #define cins(s)	        scanf("%s",s)
+#define deb(x)          cout << x << "  " ;
+#define out(x)          cout << x << endl ;
+#define endl            '\n'
+#define pb              push_back
+#define mk              make_pair
+#define fastio          ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define loop(x,n,v)     for(ll v=x; v<n; v++)
+#define loopr(n,x,v)     for(ll v=n-1; v>=x; v--)
+#define mod             1000000007
+#define all(x)          x.begin(), x.end()
+#define clr(x)          memset(x, 0, sizeof(x))
+#define sortall(x)      sort(all(x))
+#define tr(it, a)       for(auto it = a.begin(); it != a.end(); it++)
+#define PI              3.1415926535897932384626
+#define setbits(x)      __builtin_popcount(x)
+#define zrobits(x)      __builtin_ctz(x)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define w(t)            int t; cin>>t; while(t--) 
+#define     vi          vector<ll>
+#define     vtt         vector<pair<ll,ll>> 
+#define     pii         pair<ll,ll> 
+#define  mapp           map<ll,ll> 
+#define  sett           set<ll>
+int dp[1000][1000] ;
+ll mpow(ll base, ll exp); 
+void ipgraph(int m);
+void dfs(int u, int par);
+const int N = 130, M = N;
+//====================================
+//====================================
+
+vi g[N];
+int a[N][N];
+// int n;
+
+ll prme(ll n) { 
+    ll result = n; // Initialize result as n 
+  
+    // Consider all prime factors of n and subtract their 
+    // multiples from result 
+    for (ll p = 2; p * p <= n; ++p) { 
+          
+        // Check if p is a prime factor. 
+        if (n % p == 0) { 
+              
+            // If yes, then update n and result 
+            while (n % p == 0) 
+                n /= p; 
+            result -= result / p; 
+        } 
+    } 
+  
+    // If n has a prime factor greater than sqrt(n) 
+    // (There can be at-most one such prime factor) 
+    if (n > 1) 
+        result -= result / n; 
+    return result; 
+}
+
+int main(){
+    fastio ;
+    w(t)
+    {
+        ll a=0, b=0, c=0, d=0, e=0, p=0, q=0, r=0;
+        ll ans=0, res=0, cnt=0, sum=0, diff=0;
+        ll n,m ;
+        cin >> n ;
+        cin >> m ;
+        p = __gcd(m,n);
+        cout << prme(m/p) << endl ;
+    }
+    return 0 ;
+}
+
+// g++ x.cpp -o as && ./as
